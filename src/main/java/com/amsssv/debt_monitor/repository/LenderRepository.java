b31283d2aa3e836b4ee516cb24e5f2463530221e
Alexander Maltsev <shura.mv@truelabel.io>
2025-08-03 22:01:03 +0300

    add ability to add lenders

--- src/main/java/com/amsssv/debt_monitor/repository/LenderRepository.java
+++ src/main/java/com/amsssv/debt_monitor/repository/LenderRepository.java
@@ -4,9 +4,12 @@ import com.amsssv.debt_monitor.entity.Lender;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LenderRepository extends JpaRepository<Lender, Long> {
   Optional<Lender> findByName(String name);
+
+  List<Lender> findByUserId(Long id);
 }
